62a91430240154d02efc9b3dc263e2801c9c597b
William <wbrines@pdx.edu>
2018-11-11 00:20:00 -0800

    Changed logo to Power symbol
    Changed logo to Power symbol in the Display Test.

--- Programs/ArduinoPrograms/examples/displayTest/displayTest.ino
+++ Programs/ArduinoPrograms/examples/displayTest/displayTest.ino
@@ -17,24 +17,22 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #define LOGO_HEIGHT 16
-#define LOGO_WIDTH 16
+#define LOGO_WIDTH 14
 static const unsigned char PROGMEM logo_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
+{ B00000001, B10000000,
+  B00000001, B10000000,
+  B00000101, B10100000,
+  B00011101, B10110000,
+  B00110001, B10011000,
+  B00110001, B10011000,
+  B01100001, B10001100,
+  B01100001, B10001100,
+  B01100000, B00001100,
+  B00110000, B00001100,
+  B00110000, B00011000,
+  B00011000, B00011000,
+  B00001110, B01110000,
+  B00000111, B11100000 };
 
 void setup() {
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
@@ -50,7 +48,7 @@ void loop(){
 
   // Clear the buffer
   display.clearDisplay();
-
+/*
   // Draw a single pixel in white
   display.drawPixel(10, 10, WHITE);
 
@@ -175,7 +173,7 @@ void loop(){
   delay(2000);
   display.stopscroll();
   delay(1000);
-
+*/
   // Draw a small bitmap image
   display.clearDisplay();
 
@@ -184,12 +182,13 @@ void loop(){
     (display.height() - LOGO_HEIGHT) / 2,
     logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
   display.display();
-  delay(1000);
-  
+  delay(10000);
+  /*
   // Invert and restore display, pausing in-between
   display.invertDisplay(true);
   delay(1000);
   display.invertDisplay(false);
   delay(1000);
+  */
 }
 
